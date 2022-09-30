4adb981c25d48f1acc5ee9ec7c12b4e709800d95
CroneGamesPlays <77684277+CroneGamesPlays@users.noreply.github.com>
2022-09-30 08:30:43 -0300

    Add files via upload

---
+++ components/sections/app-footer.vue
@@ -0,0 +1,10 @@
+<template>
+	<footer class="body-font">
+		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+			<p class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4">
+				© 2022 CroneGamesPlays —
+				<external-link href="https://twitter.com/Felipe81063023" text="@Felipe81063023" />
+			</p>
+		</div>
+	</footer>
+</template>
---
+++ components/user/roles/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<h2 class="font-bold text-xs text-zinc-800 dark:text-slate-300 leading-4 mb-2 uppercase">Roles</h2>
+	<div aria-label="Roles" role="list" tabindex="0" class="mt-0.5 mb-3 flex flex-wrap">
+		<user-roles-item role="JavaScript" class-color="bg-yellow-400" />
+	</div>
+</template>
