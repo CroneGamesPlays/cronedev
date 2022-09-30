54c5dec8dceb5b64144c119955db074685dcf532
CroneGamesPlays <77684277+CroneGamesPlays@users.noreply.github.com>
2022-09-30 09:41:16 -0300

    Update index.vue

--- components/user/roles/index.vue
+++ components/user/roles/index.vue
@@ -2,5 +2,6 @@
 	<h2 class="font-bold text-xs text-zinc-800 dark:text-slate-300 leading-4 mb-2 uppercase">Roles</h2>
 	<div aria-label="Roles" role="list" tabindex="0" class="mt-0.5 mb-3 flex flex-wrap">
 		<user-roles-item role="JavaScript" class-color="bg-yellow-400" />
+		<user-roles-item role="HTML & CSS" class-color="bg-orange-400" />
 	</div>
 </template>
